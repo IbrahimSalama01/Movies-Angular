bc2be555456c14052f2bdbd636fc44ef13e44523
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-04-27 19:47:18 +0300

    feat: add person card component with template, styles, and logic

---
+++ src/app/shared/person-card/person-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, input, signal } from '@angular/core';
+import { IPerson } from '../../utils/Interfaces';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-person-card',
+  imports: [CardModule,ButtonModule, TagModule,NgIf],
+  templateUrl: './person-card.component.html',
+  styleUrl: './person-card.component.css'
+})
+export class PersonCardComponent {
+  person = input.required<IPerson>();
+
+}
