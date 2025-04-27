9028d7a9e7adba0e2a77b29cbbe72d24e602a875
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-04-27 19:51:17 +0300

    feat: update app structure with routing and component integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { AllComponent } from './feature/all/all.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',component : AllComponent
+  },
+  {
+    path: 'all',component : AllComponent
+  },
+  {
+    path: 'movies',
+    loadComponent: () => import('./feature/movies/movies.component').then(c=> c.MoviesComponent)
+  },
+  {
+    path: 'people',
+    loadComponent: () => import('./feature/people/people.component').then(c => c.PeopleComponent)
+  },
+  {
+    path: 'tvseries',
+    loadComponent: () => import('./feature/tvseries/tvseries.component').then(c => c.TvseriesComponent)
+  },
+  {
+    path: 'movies/:id',
+    loadComponent: () => import('./feature/movies/movie-details/movie-details.component').then(c => c.MovieDetailsComponent)
+  }
+];
