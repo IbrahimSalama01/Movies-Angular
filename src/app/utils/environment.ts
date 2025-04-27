2c4eb4fba9280f8ff2820154a2d0e59a3490f66e
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-04-27 19:45:09 +0300

    Add interfaces and environment configuration for movie API integration

---
+++ src/app/utils/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  API_KEY: '65cf887b861d6a3ed822caa51655c18e',
+  API_SECRET:'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NWNmODg3Yjg2MWQ2YTNlZDgyMmNhYTUxNjU1YzE4ZSIsIm5iZiI6MTc0NTYxMTA2OC41NjMsInN1YiI6IjY4MGJlOTNjYmZiZGYxZjhjNThhMDExNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xBilOnbG31BKlNjx_n8lUiRp_yW8UNJCIRs3MwVlJbI',
+  API_URL_Trending:"https://api.themoviedb.org/3/trending/",
+  API_Options:"/day?language=en-US",
+  API_URL_Details:"https://api.themoviedb.org/3/"
+}
