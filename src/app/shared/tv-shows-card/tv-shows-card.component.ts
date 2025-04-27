349e17d5f6f155617af74213def3b83c1202092e
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-04-27 19:50:27 +0300

    feat: add TvShowsCardComponent with template and styles for displaying TV shows

---
+++ src/app/shared/tv-shows-card/tv-shows-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tv-shows-card',
+  imports: [],
+  templateUrl: './tv-shows-card.component.html',
+  styleUrl: './tv-shows-card.component.css'
+})
+export class TvShowsCardComponent {
+
+}
