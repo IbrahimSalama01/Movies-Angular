1e8baf8c74ab3a62893deafb928ba431d676f4e4
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-04-27 19:49:19 +0300

    feat: add Footer and Navbar components with templates, styles, and logic for sidebar and theme toggling

---
+++ src/app/core/layout/navbar/navbar.component.ts
@@ -0,0 +1,45 @@
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
+import { DrawerModule } from 'primeng/drawer';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { RouterLink } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
+
+
+@Component({
+  selector: 'app-navbar',
+  imports: [DrawerModule, ButtonModule, MenuModule, RouterLink, MenubarModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent implements OnInit{
+  public sidebarVisible: boolean = false;
+  public isMobile: boolean = false;
+  public isDarkTheme: boolean = false;
+
+  ngOnInit() {
+    this.isMobile = window.innerWidth < 768;
+  }
+
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.isMobile = window.innerWidth < 768;
+  }
+
+  toggleSidebar() {
+    // Toggle the sidebar visibility
+    console.log("Sidebar toggled");
+    console.log(this.sidebarVisible);
+    this.sidebarVisible = !this.sidebarVisible;
+  }
+
+  toggleTheme() {
+    const element = document.querySelector('html');
+    element?.classList.toggle('my-app-dark');
+    console.log(element);
+    this.isDarkTheme = !this.isDarkTheme;
+
+  }
+
+}
