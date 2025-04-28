124e24f4f48df44e909e9eee9137a4a039959ed2
Ibrahim Salama <ibrahimsalama100@outlook.com>
2025-04-28 06:36:25 +0300

    refactor: comment out unused import for Aura theme in app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+// import Aura from '@primeng/themes/aura';
 import Matrial from '@primeng/themes/material';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
